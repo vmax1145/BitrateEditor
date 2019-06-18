8a1b50fffa41a17412741046db29b6f1f3dc6448
vmax1 <virtual_max@geocities.com>
2019-06-18 21:24:33 +0700

    add calculator, fix bat files

--- bitratesSJ8ProV1.2.7.bat
+++ bitratesSJ8ProV1.2.7.bat
@@ -1,20 +1,3 @@
-setlocal
-@echo off
-echo **************************************************************
-echo * Author do not take any responsibility and
-echo * is not liable for any damage or loss caused by using this software.
-echo *
-echo * !!! All you are doing is at your own risk !!!
-echo *
-echo **************************************************************
-:PROMPT
-SET /P AREYOUSURE=Accept (Y/[N])?
-IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
-
-java -jar bitrate-editor-1.0-SNAPSHOT.jar SJ8.v1.2.7.config.json
-
-
-:END
-endlocal
+java -jar bitrate-editor-1.2-SNAPSHOT.jar SJ8.v1.2.7.config.json
 
 
