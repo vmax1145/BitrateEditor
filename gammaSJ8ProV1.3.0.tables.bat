21fcba5764f1169b341c32d11c5631d8e264d2eb
vmax1 <virtual_max@geocities.com>
2019-09-21 21:50:18 +0700

    expo

---
+++ gammaSJ8ProV1.3.0.tables.bat
@@ -0,0 +1,1 @@
+java -jar firmware-editor-tool-5.0-SNAPSHOT.jar config-gamma\SJ8.v1.3.0.gamma.config.json
--- tablesSJ8ProV1.3.0.tables.bat
+++
@@ -1,1 +0,0 @@
-java -jar firmware-editor-tool-5.0-SNAPSHOT.jar config-tables\SJ8.v1.3.0.tables.config.json
