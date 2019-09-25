895262367ce7d642211799a59d49f41684185a97
vmax1 <virtual_max@geocities.com>
2019-09-25 22:33:28 +0700

    add preview image

--- bitratesSJ8ProV1.2.7.MOD1.bat
+++ bitratesSJ8ProV1.2.7.MOD1.bat
@@ -1,3 +1,3 @@
-java -jar firmware-editor-tool-5.0-SNAPSHOT.jar configs-bitrate\SJ8.v1.2.7.MOD1.config.json
+java -jar firmware-editor-tool-6.0-SNAPSHOT.jar configs-bitrate\SJ8.v1.2.7.MOD1.config.json
 
 
